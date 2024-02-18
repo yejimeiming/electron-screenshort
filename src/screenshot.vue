757da0b99a1d53ede814a864c0a0642c5e374e92
野鸡没名 <asd308487730@gmail.com>
2024-02-18 11:26:38 +0800

    chore: cleanup

--- src/screenshot.vue
+++ src/screenshot.vue
@@ -1,28 +1,9 @@
 <script setup lang="ts">
-import Screenshot from './screenshot/index.vue'
-import { Call } from './screenshot/funcs/call'
-import { Mouse } from './screenshot/funcs/mouse'
-import { points2bounds } from './screenshot/funcs/mouse.utils'
-import { useStore } from './screenshot/store'
 import { resolveImage } from './screenshot/utils/image'
+import { useStore } from './screenshot/store'
+import Screenshot from './screenshot/index.vue'
 
 const store = useStore()
-
-Call._init({
-  async onOk(blob: Blob) {
-    await navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })])
-
-    alert('已经复制截图到剪切板 ^_^')
-
-    window.ipcRenderer.invoke('exit-screenshot')
-  },
-  async onCancel() {
-    window.ipcRenderer.invoke('exit-screenshot')
-  },
-  async onSave(blob: Blob) {
-    console.log('点击保存:', blob)
-  }
-})
 
 window
   .ipcRenderer
@@ -41,31 +22,6 @@ window
       image,
     })
   })
-
-Mouse.up(args => {
-  if (store.bounds) {
-    store.setBoundsDrawn(true)
-  }
-})
-
-Mouse.move(args => {
-  const { event, mousedown } = args
-
-  // 全局鼠标坐标
-  store.setClientX(args.clientX)
-  store.setClientY(args.clientY)
-
-  if (mousedown && !store.boundsDrawn) {
-    // 按住鼠标移动 - 画框
-    store.setBounds(points2bounds(
-      { x: args.startX, y: args.startY },
-      { x: event.clientX, y: event.clientY },
-      store.width,
-      store.height,
-    ))
-  }
-})
-
 </script>
 
 <template>
--- src/screenshot/index.vue
+++ src/screenshot/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { DHistory } from './funcs/draw.history'
 import { Call } from './funcs/call'
+import { Mouse } from './funcs/mouse'
+import { points2bounds } from './funcs/mouse.utils'
 import { Canvas } from './screenshot-canvas/canvas'
 import { useStore } from './store'
 import { composeImage } from './utils/image'
@@ -56,10 +58,52 @@ const onContextMenu = (e: MouseEvent) => {
 	store.reset()
 }
 
+// ------- global init something -------
+
+Call._init({
+	async onOk(blob: Blob) {
+		await navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })])
+
+		alert('已经复制截图到剪切板 ^_^')
+
+		window.ipcRenderer.invoke('exit-screenshot')
+	},
+	async onCancel() {
+		window.ipcRenderer.invoke('exit-screenshot')
+	},
+	async onSave(blob: Blob) {
+		console.log('点击保存:', blob)
+	}
+})
+
+Mouse.up(args => {
+	if (store.bounds) {
+		store.setBoundsDrawn(true)
+	}
+})
+
+Mouse.move(args => {
+	const { event, mousedown } = args
+
+	// 全局鼠标坐标
+	store.setClientX(args.clientX)
+	store.setClientY(args.clientY)
+
+	if (mousedown && !store.boundsDrawn) {
+		// 按住鼠标移动 - 画框
+		store.setBounds(points2bounds(
+			{ x: args.startX!, y: args.startY! },
+			{ x: event.clientX, y: event.clientY },
+			store.width,
+			store.height,
+		))
+	}
+})
 </script>
 
 <template>
-	<div class="screenshot-index" :style="{ width: store.width, height: store.height }" @dblclick="onDoubleClick" @contextmenu="onContextMenu">
+	<div class="screenshot-index" :style="{ width: store.width, height: store.height }" @dblclick="onDoubleClick"
+		@contextmenu="onContextMenu">
 		<!-- 背景图、悬浮框 -->
 		<ScreenshotBackground />
 		<!-- 截图框、二次绘制 -->
@@ -79,6 +123,7 @@ const onContextMenu = (e: MouseEvent) => {
 	transform: translateZ(0);
 	cursor: crosshair;
 	font-family: $font-family;
+
 	&,
 	* {
 		box-sizing: border-box;
